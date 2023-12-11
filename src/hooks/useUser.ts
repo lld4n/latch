e6dfaf2a2cf87595f222492d41d5002cf501ad6b
lldan <safinildan@gmail.com>
2023-12-11 18:27:24 +0300

    фикс window

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,8 @@
+"use client";
 import { Id } from "../../convex/_generated/dataModel";
 
 export function useUser() {
-  if (window && localStorage.getItem("latch-user")) {
+  if (typeof window !== "undefined" && localStorage.getItem("latch-user")) {
     return localStorage.getItem("latch-user") as Id<"users">;
   } else {
     return null;
