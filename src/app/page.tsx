76377f68e0e4f23bca802859d9189cfc1c080bd0
lldan <safinildan@gmail.com>
2024-01-07 23:52:02 +0300

    стартовую страницу отредактировал

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import styles from "./page.module.scss";
 import Link from "next/link";
-import { Film } from "lucide-react";
 import React from "react";
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h1>Специальный проект для Агоны</h1>
+        <h1>Лучше смотрите фильмы/сериалы, чем новости</h1>
         <Link href="/list" className={styles.btn}>
-          Коллекция фильмов/сериалов
-          <Film size={16} />
+          Коллекция
         </Link>
       </main>
       <Footer />
